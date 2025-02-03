e6ae04a99fd306e92a649aa190202ac07a399d9d
Dang-pig <tdhieubuns.19072009@gmail.com>
2025-02-03 21:40:39 +0700

    first fix

--- index.js
+++ index.js
@@ -6,24 +6,24 @@ const Host = "http://localhost:8080";
 
 const urlDatabase = {};
 
-const Base = 691;
-const Mod = [3521614606199, 3521614606183];
+const Base = 691n;
+const Mod = [839299365868340213n, 839299365868340207n];
 
 function hashFunction(str, ModId) {
-    let ans = 0;
-    for (let i = 0; i < str.length; i++) ans = (ans * Base + str[i].charCodeAt(0)) % Mod[ModId];
+    let ans = 0n;
+    for (let i = 0; i < str.length; i++) ans = (ans * Base + BigInt(str[i].charCodeAt(0))) % Mod[ModId];
     return ans;
 }
 
 function toBase62(num) {
     const chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
     let result = '';
-    while (num > 0) {
-        result = chars[num % 62] + result;
-        num = Math.floor(num / 62);
+    while (num > 0n) {
+        result = chars[Number(num % 62n)] + result;
+        num = num / 62n;
     }
 
-    return result.padStart(7, '0');
+    return result.padStart(10, '0');
 }
 
 function shorten(url) {
@@ -31,7 +31,6 @@ function shorten(url) {
 }
 
 const Server = http.createServer((req, res) => {
-    console.log(req.url);
     if (req.url == '/shorten') {
         if (req.method === 'POST') {
             let body = '';
@@ -39,36 +38,31 @@ const Server = http.createServer((req, res) => {
 
             req.on('end', () => {
                 const url = body;
-                console.log(url);
-
                 const urlID = `/${shorten(url)}`;
                 const shortenedUrl = Host + urlID;
                 urlDatabase[urlID] = url;
-                console.log(urlDatabase);
                 res.writeHead(200, { 'Content-Type': 'application/json' });
-                console.log(JSON.stringify({ shortenedUrl }));
 
                 res.end(JSON.stringify({ shortenedUrl }));
             });
         }
         else {
-            fs.readFile("./shorten.html", (err, data) => {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(data);
-                return;
-            });
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            fs.createReadStream("./shorten.html").pipe(res);
         }
-        return;
     }
-
-    if (req.url.startsWith('/') && urlDatabase[req.url]) {
-        const originalUrl = urlDatabase[req.url];
-        console.log(originalUrl);
-
-        res.writeHead(302, { Location: originalUrl }); // Redirect to the original URL
-        res.end();
-        return;
+    else if (req.url.startsWith('/')) {
+        if (urlDatabase[req.url]) {
+            const originalUrl = urlDatabase[req.url];
+            res.writeHead(302, { Location: originalUrl });
+            res.end();
+        }
+        else{
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            fs.createReadStream("./noneexist.html").pipe(res);
+        }
     }
+    return;
 });
 
 Server.listen(8080);
